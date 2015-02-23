3633e8555d7c036cd777e42cd033bb4e82d92b65
seowyanyi <seow.yanyi@gmail.com>
2015-02-24 07:36:29 +0800

    storage skeleton code

--- MyCal/Storage.cpp
+++ MyCal/Storage.cpp
@@ -3,8 +3,29 @@
 void Storage::initStorage() {
 }
 
-void Storage::updateStorage(DS::CHANGES changes, DS::TaskList taskList) {
+DS::TaskList Storage::getAllTasks() {
+    return _sessionStore.back();
+}
 
+void Storage::updateStorage(DS::CHANGES changes, DS::TaskList taskList) {
+    _sessionStore.push_back(taskList);
+    //write to storageSettings.txt
+    //overwrite mostRecentChange.txt
+    //overwrite taskList.txt
+}
+
+DS::CHANGES Storage::undoLastAction() {
+    if (_sessionStore.size() > 1) {
+        _sessionStore.pop_back();
+        DS::CHANGES sampleChange; // = read from mostRecentChange.txt
+        //write to files again to update change
+        return sampleChange;
+    } else {
+        //throw exception
+        //
+        DS::CHANGES sampleChange;
+        return sampleChange;
+    }
 }
 
 unsigned Storage::getMaxID() {
--- MyCal/Storage.h
+++ MyCal/Storage.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <deque>
-#include "DataStructure.h"
 #include <string>
+#include "DataStructure.h"
 
 class Storage {
 public:
 	void initStorage();
-	void updateStorage(DS::CHANGES changes, DS::TaskList taskList);
-	unsigned getMaxID();
+    static DS::TaskList getAllTasks();
+	static void updateStorage(DS::CHANGES changes, DS::TaskList taskList);
+    DS::CHANGES undoLastAction();
+	static unsigned getMaxID();
 
 	Storage(void);
 	~Storage(void);
@@ -15,8 +17,8 @@ public:
 
 private:
 	typedef std::deque<DS::TaskList> Store;
-	Store _sessionStore;
-	unsigned _curMaxID;
+	static Store _sessionStore;
+	static unsigned _curMaxID;
 	std::string _taskListLoc; //tastList.txt's location
 };
 
