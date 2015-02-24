c15de3b8f96eee8d30857f6c8f2c7f6d24329e4a
seowyanyi <seow.yanyi@gmail.com>
2015-02-24 08:51:25 +0800

    command parsing skeleton

--- MyCal/Controller.cpp
+++ MyCal/Controller.cpp
@@ -1,6 +1,26 @@
+#include <sstream>
 #include "Controller.h"
 
+std::string Controller::getFirstWord(std::string userCmd) {
+	std::istringstream iss(userCmd);
+	std::string firstWord;
+	iss >> firstWord;
+	return firstWord;
+};
+
+DS::CommandType Controller::determineCmdType(std::string cmdTypeString) {
+    return DS::ADD; //example
+}
+
 DS::UIObject Controller::handleInput(std::string input) {
+    std::string cmdTypeString = getFirstWord(input);
+    DS::CommandType commandType = determineCmdType(input);
+    /*
+    switch (commandType) {
+
+    }*/
+
+    // below is just a sample uiobj. 
 	DS::TASK task;
 	task.taskID = 1;
 	task.taskName = "eat hamburger";
--- MyCal/Controller.h
+++ MyCal/Controller.h
@@ -3,6 +3,9 @@
 #include "DataStructure.h"
 
 class Controller {
+private:
+    std::string getFirstWord(std::string userCmd);
+    DS::CommandType determineCmdType(std::string cmdTypeString);
 public:
 
 	DS::UIObject handleInput(std::string input);
