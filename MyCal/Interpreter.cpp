ce4af41a8ce95719560cac099853354e2c621172
seowyanyi <seow.yanyi@gmail.com>
2015-02-24 09:48:04 +0800

    return dummy taskObj

--- MyCal/Interpreter.cpp
+++ MyCal/Interpreter.cpp
@@ -4,10 +4,17 @@ DS::TASK Interpreter::parseAddCmd(std::string task) {
     //which we can store into DS::TASK
     //tip- user needs to put colon ":" before specifying details
     //(refer to dropbox commoncommands.txt)
+
+    DS::TASK taskObj;
+    //do something
+    return taskObj;
 }
 
 DS::TASK Interpreter::parseEditCmd(std::string task) {
 
+    DS::TASK taskObj;
+    //do something
+    return taskObj;
 }
 
 Interpreter::Interpreter(void) {
