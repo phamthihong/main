4d86390e058a3564f2a431c8bdc0496cc04d5225
seowyanyi <seow.yanyi@gmail.com>
2015-03-10 08:02:29 +0800

    update for command type

--- Controller/Controller.cpp
+++ Controller/Controller.cpp
@@ -1,50 +1,42 @@
 #include <sstream>
 #include "Controller.h"
-
-std::string Controller::getFirstWord(std::string userCmd) const {
-	std::istringstream iss(userCmd);
-	std::string firstWord;
-	iss >> firstWord;
-	return firstWord;
-};
-
-DS::CommandType Controller::determineCmdType(std::string cmdTypeString) const {
-    return DS::ADD; //example
-}
+#include "CommandType.h"
 
 bool Controller::isRunning() const {
     return _isRunning;
 }
 
-DS::UIObject Controller::handleInput(std::string input) {
-    std::string cmdTypeString = getFirstWord(input);
-    DS::CommandType commandType = determineCmdType(input);
-    /*
-    switch (commandType) {
-
-    }*/
-
-    //If user exits, set _isRunning to false
-
-    // below is just a sample uiobj. 
-	DS::TASK task;
-	task.taskID = 1;
-	task.taskName = "eat hamburger";
-    time(&task.taskStart);
-    time(&task.taskEnd);
-
-	DS::SINGLE_DAY singleDay;
-    singleDay.taskDate = task.taskStart;
-	singleDay.tasksThisDay.push_back(task);
-
-	DS::TaskList myTaskList;
-	myTaskList.push_back(singleDay);
-
-	DS::UIObject sampleUIObj;
-	sampleUIObj.headerText = "Tasks for today:";
-	sampleUIObj.taskList = myTaskList;
-
-	return sampleUIObj;
+UIObject Controller::handleInput(std::string input) {
+    CommandType::Command cmdType = CommandType::determineCmdType(input);  
+    switch (cmdType) {
+        case CommandType::ADD: {
+            
+        }
+        case CommandType::DELETE: {
+        }
+        case CommandType::EDIT: {
+        }
+        case CommandType::POSTPONE: {
+        }
+        case CommandType::UNDO: {
+        }
+        case CommandType::SEARCH: {
+        }
+        case CommandType::VIEW: {
+        }
+        case CommandType::BLOCK: {
+        }
+        case CommandType::CONFIRM: {
+        }
+        case CommandType::STORAGE: {
+        }
+        case CommandType::EXIT_PROGRAM: {
+        }
+        case CommandType::INVALID: {
+        }
+    }
+    UIObject test;
+    return test;
 }
 
 Controller::Controller(void): _isRunning(true) {
--- Controller/Controller.h
+++ Controller/Controller.h
@@ -1,17 +1,15 @@
 #pragma once
 #include <string>
-#include "DataStructure.h"
+#include "UIObject.h"
 
 class Controller {
 private:
     bool _isRunning;
 
-    std::string getFirstWord(std::string userCmd) const;
-    DS::CommandType determineCmdType(std::string cmdTypeString) const;
 public:
 
     bool isRunning() const;
-    DS::UIObject handleInput(std::string input);
+    UIObject handleInput(std::string input);
     
     Controller(void);
     ~Controller(void);
